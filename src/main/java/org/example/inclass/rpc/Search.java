0fc36ed26257b7db5beeb6bc81aff82fbaaaadc9
chrisluo5311 <chrislo5311@gmail.com>
2025-05-07 11:49:46 -0700

    delete original inclass file and draw.io

--- src/main/java/org/example/inclass/rpc/Search.java
+++
@@ -1,9 +0,0 @@
-package org.example.inclass.rpc;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Search extends Remote {
-
-    public String query(String search) throws RemoteException;
-}
