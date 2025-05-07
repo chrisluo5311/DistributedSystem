126db55f6f6730e5215d5babff4c303cdb5c772e
chrisluo5311 <chrislo5311@gmail.com>
2025-05-06 18:47:42 -0700

    add scu homepage

---
+++ webSource/assets/public/scu.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4078619eb04733762b0db2a3d0067c39a247606be780bfd270bba0dedbc85168
+size 657587
---
+++ webSource/media/scuedu/style-assets/javascript/lazyloading.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:66bebfecfc6868ced846d2629ba050486b2c55abe409096effd884557c9b47f3
+size 3394
